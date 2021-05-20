0182891e1bf3e55ef088699522db837e6d145806
root <root@DKLPC0YMT19.laerdal.global>
2021-05-20 16:29:37 +0200

    heroku

--- api/app.py
+++ api/app.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def root_response():
+    response = make_response("ML-Boilerplate API. Endpoints: /api/v1.0/test")
+    return response
 
 
 @app.route('/api/v1.0/test', methods=['GET'])
@@ -16,7 +22,7 @@ def test_response():
     response = make_response(jsonify(sample_response))
 
     # Add Access-Control-Allow-Origin header to allow cross-site request
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    response.headers['Access-Control-Allow-Origin'] = os.environ.get('GRANT_CORS_ACCESS')
 
     # Mozilla provides good references for Access Control at:
     # https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
--- deploy_heroku.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-heroku container:login
-heroku create laerdal-ml-boilerplate
-docker tag ml-boilerplate/api registry.heroku.com/laerdal-ml-boilerplate/web
-docker push registry.heroku.com/laerdal-ml-boilerplate/web
-heroku container:release web
-heroku open
